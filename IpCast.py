f8c18a4fce1e305edb3e3bae890367653a87e3d4
RunningAroundInCircles <karsten@raic.dk>
2017-07-17 14:07:52 +0200

    Testet and Working Ip and Name broadcaster on local network for Raspberry Pi

---
+++ IpCast.py
@@ -0,0 +1,40 @@
+import time
+import socket
+import fcntl
+import struct
+import sys
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+UDP_IP = "255.255.255.255"
+UDP_PORT = 5005
+
+def getIpAddress(network):
+	return socket.inet_ntoa(fcntl.ioctl(
+		s.fileno(),
+		0x8915,  # SIOCGIFADDR
+		struct.pack('256s', network[:15])
+	)[20:24])
+
+hostname = socket.gethostname()
+ip = (getIpAddress('wlan0'.encode('utf-8')))
+
+MESSAGE = "Pi name: " + hostname + ", IP: " + ip
+
+count = 12
+msg = 1
+
+print("Broadcasting Raspberry Pi info:")
+while count > 0:
+	s.sendto(bytes(MESSAGE,'utf-8'),(UDP_IP, UDP_PORT))
+	print("Message " + str(msg) + " Send")
+	time.sleep(5)
+	count -= 1
+	msg += 1
+
+s.close
+
+print("Broadcaster closed")
+
+sys.exit()
